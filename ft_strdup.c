fb4bd7b42c11257cd2c5e06f69a0818ffdf7b8c0
Baadeeya <david14av@gmail.com>
2020-10-05 19:24:35 +0200

    strdup fix

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,32 +6,22 @@
 /*   By: dgutin <dgutin@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/05 19:04:12 by dgutin            #+#    #+#             */
-/*   Updated: 2020/10/05 19:18:57 by dgutin           ###   ########.fr       */
+/*   Updated: 2020/10/05 19:24:18 by dgutin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "libft.h"
-#include <stdlib.h>
-#include <string.h>
+#include "libft.h"
 
 char	*ft_strdup(const char *src)
 {
-	int	i;
+	int		i;
 	char	*dest;
 
 	i = 0;
 	if (!(dest = malloc((strlen(src) + 1) * sizeof(char))))
 		return (NULL);
 	while (*src && src)
-		dest[i] = *src++;
+		dest[i++] = *src++;
 	dest[i] = '\0';
 	return (dest);
-}
-
-#include <stdio.h>
-
-int	main(void)
-{
-	printf("%s", ft_strdup("Salut\n"));
-	return (0);
 }
