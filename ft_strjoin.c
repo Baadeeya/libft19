6e3d53f43a4e725bf6699b4a6dc79231a7b9e880
Baadeeya <david14av@gmail.com>
2020-10-05 19:15:23 +0200

    added some + working on strdup

---
+++ ft_strjoin.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dgutin <dgutin@student.s19.be>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/05 18:53:35 by dgutin            #+#    #+#             */
+/*   Updated: 2020/10/05 18:54:38 by dgutin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_bigfatstrlen(int size, char **strs, char *sep)
+{
+	int		x;
+	int		y;
+	int		m;
+
+	x = 0;
+	m = 0;
+	while (sep[m])
+		m++;
+	m *= size - 1;
+	while (x < size)
+	{
+		y = 0;
+		while (strs[x][y])
+		{
+			y++;
+			m++;
+		}
+		x++;
+	}
+	if (m <= 0)
+		return (0);
+	return (m + 1);
+}
+
+char	*ft_strjoin(int size, char **strs, char *sep)
+{
+	int		x;
+	int		y;
+	int		i;
+	int		s;
+	char	*str;
+
+	if (!(str = malloc(sizeof(char) * ft_bigfatstrlen(size, strs, sep))))
+		return (0);
+	if (size == 0)
+		return (str);
+	x = -1;
+	i = -1;
+	while (++x < size)
+	{
+		y = -1;
+		s = -1;
+		while (strs[x][++y])
+			str[++i] = strs[x][y];
+		while (sep[++s] && x < size - 1)
+			str[++i] = sep[s];
+	}
+	str[++i] = '\0';
+	return (str);
+}
