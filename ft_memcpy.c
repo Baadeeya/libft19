98dd9ddb111fa0b525d468056973748537958f6d
David Gutin <david14av@gmail.com>
2020-11-17 13:09:46 +0100

    mem functions start

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,9 +6,20 @@
 /*   By: dgutin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 12:44:39 by dgutin            #+#    #+#             */
-/*   Updated: 2020/11/17 12:44:53 by dgutin           ###   ########.fr       */
+/*   Updated: 2020/11/17 13:07:05 by dgutin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+void	*memcpy(void *des, const void *src, size_t size)
+{
+	size_t		i;
+	const char	psrc;
+	char		pdest;
+
+	i = -1;
+	psrc = src;
+	pdest = dest;
+
+}
