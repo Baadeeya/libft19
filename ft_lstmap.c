8fc0e6d10848a5f6404bcb1fc225cc9c2d67d438
David Gutin <david14av@gmail.com>
2020-11-27 13:22:38 +0100

    wip lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: dgutin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/27 12:06:01 by dgutin            #+#    #+#             */
-/*   Updated: 2020/11/27 12:06:56 by dgutin           ###   ########.fr       */
+/*   Updated: 2020/11/27 13:22:16 by dgutin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,18 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f) (void *), void (*del)(void *))
 {
+	t_list	*list;
+
+	if (!lst || !del)
+		return (NULL);
+	if (!f)
+		return (lst);
+	if (!(list = ft_lstnew(lst->content)))
+	{
+		free(lst);
+		return (NULL);
+	}
+	list = lst;
+	ft_lstiter(list, (void*)(*f));
+	return (list);
 }
