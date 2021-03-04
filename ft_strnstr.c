1c5e72a028533691fd9c34106adfccac13651018
David Gutin <dgutin@mi-r1-p1.s19.be>
2021-03-04 15:59:46 +0100

    non

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: dgutin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/24 16:31:50 by dgutin            #+#    #+#             */
-/*   Updated: 2020/12/03 14:35:44 by dgutin           ###   ########.fr       */
+/*   Updated: 2021/03/04 15:02:44 by dgutin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t		i;
-	const char	*a;
-	const char	*f;
+	char	*a;
+	char	*f;
 
 	i = -1;
-	a = needle;
-	f = haystack;
+	a = (char *)needle;
+	f = (char *)haystack;
 	if (*needle == 0)
 		return ((char *)haystack);
 	while (++i <= len)
@@ -36,4 +36,13 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 		f++;
 	}
 	return (0);
+}
+
+int	main(void)
+{
+	size_t len = 11;
+	const char needle[] = "les";
+	const char haystack[] = "salut les amis";
+
+	printf("n = %s -- h = %s -- ret =  %s\n" needle, haystack, ft_strnstr(haystack, needle, len));
 }
